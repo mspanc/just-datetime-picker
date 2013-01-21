15f5deb773a1a04020d5f215e2b7864b8975a8b3
Marcin Lewandowski <marcin@saepia.net>
2013-01-21 19:52:49 +0100

    Fixed nested form workaround - do not use obsolete jQuery live() syntax

--- app/assets/javascripts/just_datetime_picker/nested_form_workaround.js
+++ app/assets/javascripts/just_datetime_picker/nested_form_workaround.js
@@ -1,4 +1,11 @@
-$('body.active_admin a.button').live('click', function() {
+// Workaround that enables date picker for fields in nested forms.
+// By default ActiveAdmin enables date picker once, after the page
+// has been loaded, so all datepicker fields created afterwards
+// are dead by default.
+//
+// We attach to a.button to catch a moment where user can potentially
+// add new field like this in a nested form.
+$(document).on('click', 'body.active_admin a.button', function() {
   $('input.datepicker:not(.hasDatepicker)').datepicker();
 });
 
