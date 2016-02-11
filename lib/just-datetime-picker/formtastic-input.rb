609f038911ec7dc11e9cbd458f60b043b44b24bb
yhirano <yhirano@aiming-inc.com>
2016-02-11 11:58:05 +0900

    Modify Ruby 1.9 hash syntax

--- lib/just-datetime-picker/formtastic-input.rb
+++ lib/just-datetime-picker/formtastic-input.rb
@@ -1,12 +1,12 @@
 module Formtastic
   module Inputs
-    class JustDatetimePickerInput 
+    class JustDatetimePickerInput
       include ::Formtastic::Inputs::Base
 
       def to_html
         input_wrapping do
           combined_value = builder.object.send(method)
-          
+
           hour_value_raw = builder.object.send("#{method}_time_hour")
           if not hour_value_raw.nil?
             hour_value = hour_value_raw
@@ -27,13 +27,27 @@ module Formtastic
 
           hour_value   = sprintf("%02d", hour_value)
           minute_value = sprintf("%02d", minute_value)
-          
-          label_html <<
-          builder.text_field("#{method}_date", input_html_options.merge({ :class => "just-datetime-picker-field just-datetime-picker-date datepicker", :value => builder.object.send("#{method}_date"), :maxlength => 10, :size => 10 })) <<
 
-          builder.text_field("#{method}_time_hour", input_html_options.merge({ :class => "just-datetime-picker-field just-datetime-picker-time just-datetime-picker-time-hour", :value => hour_value, :maxlength => 2, :size => 2 })) <<
+          label_html <<
+          builder.text_field("#{method}_date", input_html_options.merge(
+            class: "just-datetime-picker-field just-datetime-picker-date datepicker",
+            value: builder.object.send("#{method}_date"),
+            maxlength: 10,
+            size: 10
+          )) <<
+          builder.text_field("#{method}_time_hour", input_html_options.merge(
+            class: "just-datetime-picker-field just-datetime-picker-time just-datetime-picker-time-hour",
+            value: hour_value,
+            maxlength: 2,
+            size: 2
+          )) <<
           ":" <<
-          builder.text_field("#{method}_time_minute", input_html_options.merge({ :class => "just-datetime-picker-field just-datetime-picker-time just-datetime-picker-time-minute", :value => minute_value, :maxlength => 2, :size => 2 }))
+          builder.text_field("#{method}_time_minute", input_html_options.merge(
+            class: "just-datetime-picker-field just-datetime-picker-time just-datetime-picker-time-minute",
+            value: minute_value,
+            maxlength: 2,
+            size: 2
+          ))
         end
       end
     end
